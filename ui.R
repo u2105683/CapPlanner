c8b06f97c59ec249a42aba47ea6dfd033e7001b4
u2105683 <101791683+u2105683@users.noreply.github.com>
2022-06-20 16:38:15 +0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,30 @@
+library(shiny)
+library(dplyr)
+library(DT)
+library(shinyWidgets)
+
+
+
+ui <- fluidPage(
+  # Function to download selected records
+  downloadButton("download", "Download.csv"),
+  align="left",
+  fluidRow(
+    column("",
+           width = 10, offset = 1,
+           tags$h3("PTM Server Capacity Planner"),
+           # Display Panel for users to choose selected score
+           panel(
+             sliderInput("storage", "Storage Score",
+                         min =10, max = 100, value = 20),
+             sliderInput("memory", "Memory Score",
+                         min = 0, max = 100, value = 20),
+             sliderInput("cpu", "CPU",
+                         #min = 18, max = max(df$age), value = c(18,24)),
+                         min = 0, max = 100, value = 20),
+             
+           ), 
+           DT::dataTableOutput("table")
+    )
+  )
+)
